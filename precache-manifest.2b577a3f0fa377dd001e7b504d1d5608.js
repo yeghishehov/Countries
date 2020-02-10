c11b5fd6123c903a39cbf1695ee1d630dc696aba
yeghishehov <yeghishehov.1@gmail.com>
2020-02-10 23:21:26 +0400

    Updates

---
+++ precache-manifest.2b577a3f0fa377dd001e7b504d1d5608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927de036695e413120e146cbf7d2d242",
+    "url": "/Countries/index.html"
+  },
+  {
+    "revision": "4ea922427dc7b7eb1309",
+    "url": "/Countries/static/css/main.72cc9493.chunk.css"
+  },
+  {
+    "revision": "b678544e0075a1050a2f",
+    "url": "/Countries/static/js/2.edbd52c2.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Countries/static/js/2.edbd52c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ea922427dc7b7eb1309",
+    "url": "/Countries/static/js/main.d66f9676.chunk.js"
+  },
+  {
+    "revision": "f5683935d830356f831a",
+    "url": "/Countries/static/js/runtime-main.349e7165.js"
+  }
+]);
